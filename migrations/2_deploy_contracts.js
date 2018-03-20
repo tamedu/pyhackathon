f1fcb6b8ccae5a9ae39322cf24fc3f53f94c5cba
Alex Nguyen <dungtn@gmail.com>
2018-03-20 17:00:19 +0700

    deploy BurnablePayment locally but fail

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
 var GrantableUpdates = artifacts.require("GrantableUpdates");
 
+var BurnablePaymentFactory = artifacts.require("BurnablePaymentFactory");
+var BurnablePayment = artifacts.require("BurnablePayment");
+
 module.exports = function(deployer) {
-  deployer.deploy(GrantableUpdates);
+  // deployer.deploy(GrantableUpdates);
+  deployer.deploy(BurnablePaymentFactory);
+  deployer.deploy(BurnablePayment);
 };
