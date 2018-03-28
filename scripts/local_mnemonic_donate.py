5ab3d762268341410928eb60e45d24dea04df46f
Alex Nguyen <dungtn@gmail.com>
2018-03-29 06:25:54 +0700

    recheck & refined but still got ValueError: {'code': -32000, 'message': 'insufficient funds for gas * price + value'}

--- scripts/local_mnemonic_donate.py
+++ scripts/local_mnemonic_donate.py
@@ -22,15 +22,19 @@ import os, sys
 dir_path = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(0, dir_path + '/../cptools')
 from utils import loadABI
-from accounts import buildSignSendTx
+from accounts import buildSignSendTx, initLocalAccount, LOCAL_STORAGE
 
 from web3 import Web3, IPCProvider, HTTPProvider
 
 '''
 seed = "legal winner thank year wave sausage worth useful legal winner thank yellow"
-ganache-cli --account="0xb1b314d4fedc41fa409d26eb15e4ea1b213a3e7951dd16d8701a35c783a4a594,999988880000000000000" -i 1 -l 990000000000
+ganache-cli --account="0xb1b314d4fedc41fa409d26eb15e4ea1b213a3e7951dd16d8701a35c783a4a594,999988880000000000000" -i 1 -l 10000000000
 ./myenv/bin/populus deploy --chain ganache Donator --no-wait-for-sync
 '''
+import binascii
+initLocalAccount()
+print(binascii.hexlify(LOCAL_STORAGE['private_key']).decode('ascii'))
+print(LOCAL_STORAGE['account'].address)
 
 web3 = Web3(HTTPProvider("http://127.0.0.1:8545"))
 ADDRESS = "0xb87F1005FcCc76D6b9560A16085bE2c7d405332c"
@@ -43,7 +47,7 @@ print(donator.functions.donationsCount().call())
 print(donator.functions.donationsTotal().call())
 
 
-buildSignSendTx(donator.functions.donate, 3, value=5*(10**18), gas=99000)
+buildSignSendTx(donator.functions.donate, 10, value=5*(10**18), gas=990000)
 
 print(donator)
 print(donator.functions.donationsCount().call())
